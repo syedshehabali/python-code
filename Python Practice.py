46f1cd2cf79adf1d088388223eea3ed156c3d962
syedshehabali <110110368+syedshehabali@users.noreply.github.com>
2022-08-08 11:17:42 +0500

    Add files via upload

---
+++ Python Practice.py
@@ -0,0 +1,128 @@
+# # print("Hello World")
+# name = input("what is your name? ")
+# print("Hi " + name)
+# color = input("what is your favorite color? ")
+# print(name + " likes "+ color + " color")
+# birth_year = int(input("what is your birth year? "))
+# print(2019 - birth_year)
+# course = 'Python for "Begin
+# email = """Hi, John,
+
+# We hope your business is flurshing and doing well.
+
+# Looking forward to hear from you.
+
+# Thanking you"""
+# print(email)
+# word = "this is a boy"
+# # print(word)
+# num = 25
+# print(num)
+# num = 45
+# print(num)
+# name = input("what is your name? ")
+# age = str(input("what is your age? "))
+# print(f"{name}" + {age})
+
+# i = 1
+# while i <= 10 :
+#     print(f"{i}.  Hello World")
+#     i = i + 1
+
+# total = 0
+# i = 1
+# while i <= 10:
+#     total = total + i
+#     i += 1
+# print(total)
+
+# sum = 0
+# i = 1
+# n = int(input("Please enter your required digit: "))
+# while i <= n:
+#     sum = sum + i
+#     i += 1
+# print(sum)
+
+# total = 0
+# i = 0
+# number = (input("Please enter your number: "))
+# while i < len(number):
+#     total = total + int(number[i])
+#     i += 1
+# print(total)
+# i = 1
+# for i in range (2, 11):
+#     print(f"Hello World {i}")
+#     i += 1
+# for i in range (2,20,2):
+#     print(i)
+# for i in range (20,2,-2):
+#     print(i)
+
+# def add_two_digits(x,y):
+#     return (x + y)
+
+# total = add_two_digits (150,200)
+# 
+# print(total)
+
+#######   User Enter Name with First letter the Last Letter   #######
+
+# letter = input("Please enter your word: ")
+# print(letter)
+
+# print(f"{letter[1:]}{letter[0]}" + "ay")
+
+# letter = input("Please enter your word: ")
+
+# if len(letter)> 0 and (letter).isalpha():
+#     print(letter.lower())
+# else:
+#     print("You have not entered anything")
+
+# def sum_of_two_digits(x, y):
+#     return x + y
+# print(sum_of_two_digits(5, 6))
+# print(sum_of_two_digits(250,500))
+
+# def square(x):
+#     return (x ** 2)
+# print(square(5))
+
+# def bigger_number(x, y):
+#     if x > y:
+#         print(f"{x} is greater than {y}")
+#     else:
+#         print(f"{x} is less than {y}")
+# print(bigger_number(500, 75))
+# print(bigger_number(200,805))
+
+# my_message = "Hello World, I am a programmer."
+# print(my_message.lower())
+# print(my_message.upper())
+# print(my_message.count("l"))
+# print(my_message.find("hello"))
+# new_message = my_message.replace("world", "Universe")
+# print(new_message)
+# print(dir(my_message))
+
+# print(help(str))
+
+# digit = 5.168
+# print(round(5.168, 1))
+
+# courses = ["history", "Urdu", "physics", "Biology"]
+# print(courses)
+# print(courses[1])
+# courses.append("pushto")
+# courses.insert(2, "Samad")
+# print(courses)
+# numbers = [1,2,4,3,5]
+# print(sum(numbers))
+
+# ali  = ("Pakistan", "PIAIC", "Python")
+# print(ali, sep="+")
+
+# ali = ("Hello World", "Hello World")
+# print(ali, end= "")
